209d519b1c66a4ffbc001ca224b599cde0e9b406
BACHAM MANOJ KUMAR REDDY <141983293+BachamManoj@users.noreply.github.com>
2025-12-09 14:41:44 +0530

    Update SecurityConfig.java

--- src/main/java/com/klu/OnlineMedicalAppointment/config/SecurityConfig.java
+++ src/main/java/com/klu/OnlineMedicalAppointment/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("http://localhost:3000"));
+        cfg.setAllowedOrigins(List.of("http://localhost:3000","https://sdp-java-myc2ji0ww-manojs-projects-cb9c59bb.vercel.app/"));
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setExposedHeaders(List.of("Authorization"));
